5e9537de0795195ca8c20a265ffb162589159505
includeshubhamgenius <109446081+includeshubhamgenius@users.noreply.github.com>
2026-04-05 20:45:10 +0530

    Update contact & pricing info for Razorpay KYC

--- push6.ps1
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "Add Razorpay compliance landing page"
-git push origin main
---
+++ push_kyc.ps1
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Update contact & pricing info for Razorpay KYC"
+git push origin main
